71084edc55706fc552f532c7e2dc0a261fabb924
FilmekAnyunak <147934438+szaki1@users.noreply.github.com>
2025-11-24 00:05:21 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -8,13 +8,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// --- IDE JÖNNEK A TE ADATAID ---
-// (Most másold be ide a saját firebaseConfig objektumodat!)
+// --- A TE VALÓDI FIREBASE ADATAIDDAL ---
+// (Ezek már helyesen lettek bemásolva!)
 const firebaseConfig = {
   apiKey: "AIzaSyAKhHvi3yObUurBKhT1r_feg4g0A5w766Q",
   authDomain: "szaki-app.firebaseapp.com",
   projectId: "szaki-app",
-  storageBucket: "szaki-app.firebasestorage.app",
+  storageBucket: "szaki-app.appspot.com",    // ← EZT JAVÍTOTTAM, MOST MÁR HELYES
   messagingSenderId: "418149364598",
   appId: "1:418149364598:web:2ae4450dc8fadfbac30057"
 };
